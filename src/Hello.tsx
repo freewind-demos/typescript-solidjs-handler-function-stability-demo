b0e9f16c2623190b1b1c013ebfff458d3aa25235
Freewind <nowindlee@gmail.com>
2024-11-19 22:03:47 +0800

    typescript-react-handler-function-stability-demo

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Hello.pcss';
 
 type Props = {};
 
-export const Hello: FC<Props> = ({}) => {
+const users = ['AAA', 'BBB', 'CCC']
+export const Hello: FC<Props> = ({ }) => {
+  const [, update] = useState(0)
+  const onClick = (userName: string, timestamp: number) => {
+    console.log('onClick: ', userName, timestamp);
+  };
+
+  const someUsers = users.slice(0, Date.now() % 4);
+
   return <div className={'Hello'}>
     <h1>Hello React</h1>
+    <button onClick={() => update(Date.now())}>Update ({someUsers.length})</button>
+    <hr />
+    {someUsers.map((user, index) => <MyButton key={index} onClick={() => onClick(user, Date.now())} />)}
   </div>;
 }
+
+const MyButton: FC<{ onClick: () => void }> = React.memo(({ onClick }) => {
+  console.log(`### > MyButton${Date.now()}`, { onClick })
+  const renderCount = React.useRef(0)
+  renderCount.current += 1
+
+  return <button onClick={onClick}>Render Count: {renderCount.current}</button>
+})
